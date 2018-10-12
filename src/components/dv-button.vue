3f5b8e028ddfeac1c8dc6a450cc3aec8af390a32
Fabio Cereda <fcereda@gmail.com>
2018-10-12 12:00:34 -0300

    Nested all CSS in dv-button to make it easier to read

--- src/components/dv-button.vue
+++ src/components/dv-button.vue
@@ -93,121 +93,122 @@ export default {
   font-family: inherit;
   height: $form-control-height;
   margin-left: 0.4em;
-}  
 
-.no-border {
-    border: 1px solid transparent;
-}  
-  
-.no-border:focus {
-    border: 1px solid transparent;
-    box-shadow: 0px 0px 0px 0px;
-}  
-  
-.wide {
-    min-width: 10em;
-}  
-  
-.tall {
-    height: 4.8em !important;
+  &.no-border {
+      border: 1px solid transparent;
   }  
-  
-.short {
-    height: 2em;
-    line-height:80%;
+    
+  &.no-border:focus {
+      border: 1px solid transparent;
+      box-shadow: 0px 0px 0px 0px;
+  }  
+    
+  &.wide {
+      min-width: 10em;
+  }  
+    
+  &.tall {
+      height: 4.8em !important;
+  }  
+    
+  &.short {
+      height: 2em;
+      line-height:80%;
+  }
+
+  &.circle {
+      min-width: $form-control-height !important;
+      height: $form-control-height;
+      padding-left: 2px;
+      padding-right: 2px;
+      text-align: center;
+      border-radius: 50em;
+   }  
+
+  &.pill {
+      border-radius: 50em;
+  }  
+    
+  &.small {
+      font-size:0.75em;
+  }  
+   
+  &.big {
+      font-size:1.5em;
+  }  
+    
+   
+  &:hover {
+      filter: drop-shadow(0px 3px 3px gray);
+  }  
+        
+  &:active,
+  &.active {
+      transform: translateY(1px);
+      box-shadow: inset 0px 2px 2px 2px rgba(0,0,0,0.15) !important;
+      filter: none;
   }  
 
-.circle {
-    min-width: $form-control-height !important;
-    height: $form-control-height;
-    padding-left: 2px;
-    padding-right: 2px;
-    text-align: center;
-    border-radius: 50em;
- }  
+  &:focus {
+      border: 1px solid rgba(0, 0, 0, 0.4);
+      box-shadow: 0px 0px 0px 1px rgba(0,0,0,0.25);
+  }    
 
-.pill {
-    border-radius: 50em;
-}  
+  &.noFocus:focus {
+      border: 1px solid rgba(0, 0, 0, 0.15);
+      box-shadow: 0px 0px 0px 0px;
+  }    
+
+  &.primary,
+  &.danger,
+  &.error,
+  &.success,
+  &.warning {
+    color: white;
+    font-weight: 700;
+  }
+
+  &.primary {
+      background-color: $bg-color-primary;
+  }
+
+  &.secondary {
+    background-color: $bg-color-secondary;
+    color: white; 
+  }
+
+  &.warning {
+      background-color: $bg-color-warning;
+  }  
+
+  &.danger {
+    background-color: $bg-color-danger;
+  }
+
+  &.error {
+    background-color: $bg-color-danger;
+  }
+
+  &.success {
+    background-color: $bg-color-success;
+  }
+
+  &.info {
+    background-color: $bg-color-info;
+  }
+
+  &.dark {
+    background-color: $bg-color-dark;
+  }
+
+}
+
   
-.small {
-    font-size:0.75em;
- }  
+
+  
  
-.big {
-    font-size:1.5em;
-  }  
-  
-  
-.dv-button:hover {
-    filter: drop-shadow(0px 3px 3px gray);
-}  
-      
-.dv-button:active,
-.dv-button.active {
-    transform: translateY(1px);
-    box-shadow: inset 0px 2px 2px 2px rgba(0,0,0,0.15) !important;
-    filter: none;
-}  
 
-.btn-group > .dv-button.active {
-    transform: none;
-    filter: brightness(85%);
-}  
-  
 
-.dv-button:focus {
-    border: 1px solid rgba(0, 0, 0, 0.4);
-    box-shadow: 0px 0px 0px 1px rgba(0,0,0,0.25);
-}    
-
-.noFocus:focus {
-    border: 1px solid rgba(0, 0, 0, 0.15);
-    box-shadow: 0px 0px 0px 0px;
-}    
-  
- 
-.dv-button.primary,
-.dv-button.danger,
-.dv-button.error,
-.dv-button.success,
-.dv-button.warning {
-	color: white;
-	font-weight: 700;
-}
-
-.dv-button.primary {
-    background-color: $bg-color-primary;
-}
-
-.dv-button.secondary {
-	background-color: $bg-color-secondary;
-	color: white;	
-}
-
-.dv-button.warning {
-    background-color: $bg-color-warning;
-}  
-
-.dv-button.danger {
-	background-color: $bg-color-danger;
-}
-
-.dv-button.error {
-	background-color: $bg-color-danger;
-}
-
-.dv-button.success {
-	background-color: $bg-color-success;
-}
-
-.dv-button.info {
-	background-color: $bg-color-info;
-}
-
-.dv-button.dark {
-	background-color: $bg-color-dark;
-}
 
   
 </style>
